a64504ddb2fadb5537749de4b21a6ed8a05ecef0
AlexandreMTH <alexandretortora@hotmail.com>
2020-10-03 01:16:36 -0300

    Created EnemyState template

---
+++ Assets/Scripts/Enemies/ES_Idle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ES_Idle : EnemyState
+{
+    public override void OnEnterState()
+    {
+        base.OnEnterState();
+    }
+
+    public override void OnStateUpdate()
+    {
+        base.OnStateUpdate();
+    }
+
+    public override void OnExitState()
+    {
+        base.OnExitState();
+    }
+}
