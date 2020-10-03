965ce3b608fdc1944aecaa6a52106b365ef43066
Fabricio <fabricio-gf@hotmail.com>
2020-10-03 00:39:22 -0300

    Created a few folders, menu and loading scenes and basic layouts

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour
+{
+
+    public Animator menuAnimator;
+
+    public GameObject mainCanvas;
+    public GameObject creditsCanvas;
+    
+    
+    private static readonly int OpenCredits = Animator.StringToHash("OpenCredits");
+
+    public void PressPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PressCredits()
+    {
+        mainCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void PressBack()
+    {
+        mainCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+}
