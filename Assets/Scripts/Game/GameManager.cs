233bcc45ac833b9028362a4bede2ecb7c90e665b
Fabricio <fabricio-gf@hotmail.com>
2020-10-03 18:06:23 -0300

    Created Game scene and added tiles to tilemap

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+}
