7c7c93764f6004d5142381673a0f61fb543e6573
Rafael <rtauefp@gmail.com>
2020-10-03 12:33:14 -0300

    Added roll function to character 2d script

--- Assets/Scripts/Character2DController.cs
+++ Assets/Scripts/Character2DController.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 public class Character2DController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rollSpeed = 20f;
-    private float _rollSpeed;
+    public float startRollSpeed = 40f;
+    private float rollSpeed;
+    public float startRollCooldown = 1f;
+    private float rollCooldown;
 
     public Rigidbody2D rbd;
 
     private Vector2 movement;
-    private bool dodgeRoll;
-
     private Vector2 rollDir;
+
+    private bool dodgeRoll;
+    private bool canRoll = true;
 
     [SerializeField]
     private CharacterState charState;
@@ -29,7 +32,7 @@ public class Character2DController : MonoBehaviour
         if (rbd == null)
             rbd = GetComponent<Rigidbody2D>();
 
-        charState = CharacterState.Normal;
+        Init();
     }
 
     private void Update()
@@ -48,40 +51,75 @@ public class Character2DController : MonoBehaviour
                 HandleDodgeRoll();
                 break;
         }
+
+        HandleDodgeCooldown();
     }
 
     void GetInput()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Jump"))
             dodgeRoll = true;
+        else if (Input.GetButtonUp("Jump"))
+            dodgeRoll = false;
     }
 
     void HandleMovement()
     {
-        if (dodgeRoll)
-        {
-            dodgeRoll = false;
-
-            charState = CharacterState.DodgeRolling;
-            rollDir = (Input.mousePosition - Camera.main.WorldToScreenPoint(rbd.position)).normalized;
-            _rollSpeed = rollSpeed;
-            Debug.Log(rollDir);
-        }
-
         rbd.MovePosition(rbd.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (dodgeRoll && canRoll)
+        {
+            canRoll = false;
+
+            rollSpeed = startRollSpeed;
+
+            if (movement != Vector2.zero)
+            {
+                rollDir = movement.normalized;
+            }
+            else
+            {
+                rollDir = (Input.mousePosition - Camera.main.WorldToScreenPoint(rbd.position)).normalized;
+            }
+
+            ChangeChararacterStateTo(CharacterState.DodgeRolling);
+        }
     }
 
     void HandleDodgeRoll()
     {
-        rbd.MovePosition(rbd.position + rollDir * _rollSpeed * Time.fixedDeltaTime);
-        //transform.position += rollDir * _rollSpeed * Time.deltaTime;
+        rbd.MovePosition(rbd.position + rollDir * rollSpeed * Time.fixedDeltaTime);
 
-        _rollSpeed -= rollSpeed * 10f * Time.fixedDeltaTime;
-        if (_rollSpeed < 5f)
+        rollSpeed -= rollSpeed * 10f * Time.fixedDeltaTime;
+        if (rollSpeed < 5f)
         {
-            charState = CharacterState.Normal;
+            ChangeChararacterStateTo(CharacterState.Normal);
         }
+    }
+
+    void HandleDodgeCooldown()
+    {
+        if (!canRoll)
+        {
+            rollCooldown -= Time.fixedDeltaTime;
+            if (rollCooldown <= 0)
+            {
+                rollCooldown = startRollCooldown;
+                canRoll = true;
+            }
+        }
+    }
+
+    void ChangeChararacterStateTo(CharacterState characterState)
+    {
+        charState = characterState;
+    }
+
+    void Init()
+    {
+        ChangeChararacterStateTo(CharacterState.Normal);
+        rollCooldown = startRollCooldown;
     }
 }
