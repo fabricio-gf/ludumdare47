bc2e345d9fc246eabb51f8b59024a526e6d982e6
Fabricio <fabricio-gf@hotmail.com>
2020-10-04 11:46:28 -0300

    Added fonts and polished menu functionalities

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -1,30 +1,71 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuBehavior : MonoBehaviour
 {
 
     public Animator menuAnimator;
 
+    [Header("Canvases")]
     public GameObject mainCanvas;
+    public GameObject instructionsCanvas;
     public GameObject creditsCanvas;
-    
-    
-    private static readonly int OpenCredits = Animator.StringToHash("OpenCredits");
-    
+    public GameObject blackScreenCanvas;
+    public GameObject backButton;
 
+    [Header("Button Images")] 
+    public Image instructionsButton;
+    public Image creditsButton;
+
+    [Header("Sprites")]
+    public Sprite pressedButton;
+    public Sprite unpressedButton;
+
+    public void PressInstructions()
+    {
+        bool state = !instructionsCanvas.activeSelf;
+        backButton.SetActive(state);
+        instructionsCanvas.SetActive(state);
+        
+        instructionsButton.sprite = state ? pressedButton : unpressedButton;
+    }
+    
     public void PressCredits()
     {
-        mainCanvas.SetActive(false);
-        creditsCanvas.SetActive(true);
+        bool state = !creditsCanvas.activeSelf;
+        backButton.SetActive(state);
+        creditsCanvas.SetActive(state);
+
+        creditsButton.sprite = state ? pressedButton : unpressedButton;
     }
 
     public void PressBack()
     {
-        mainCanvas.SetActive(true);
+        backButton.SetActive(false);
         creditsCanvas.SetActive(false);
+        instructionsCanvas.SetActive(false);
+
+        instructionsButton.sprite = unpressedButton;
+        creditsButton.sprite = unpressedButton;
+    }
+
+    public void PressQuit()
+    {
+        blackScreenCanvas.SetActive(true);
+        StartCoroutine(QuitDelay());
+    }
+
+    IEnumerator QuitDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
 }
