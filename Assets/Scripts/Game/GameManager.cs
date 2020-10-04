0db070a251a11924848dbb3e731eec268bb6c1f6
Fabricio <fabricio-gf@hotmail.com>
2020-10-04 19:36:22 -0300

    Updated upgrades descriptions

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -190,6 +190,7 @@ public class GameManager : MonoBehaviour
 
             Animator playerAnimator = player.GetComponent<Animator>();
             playerAnimator.updateMode = AnimatorUpdateMode.UnscaledTime;
+            
             playerAnimator.Play("Dying");
             
             StartCoroutine(EndRoundDelay());
@@ -239,6 +240,11 @@ public class GameManager : MonoBehaviour
         remainingTime = startLoopTimeInSeconds;
         timerBarImg.fillAmount = 1;
         
+        player.GetComponent<Character2DController>().ResetVelocity();
+        player.GetComponent<Animator>().Play("Idle");
+        
+        //delete enemies
+        //reset spawner
         
         //
 
