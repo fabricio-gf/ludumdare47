397181a16ac8c72c23b90fef0db4a1a60d86242d
Fabricio <fabricio-gf@hotmail.com>
2020-10-05 17:55:46 -0300

    Added cutscene to the start of the game

--- Assets/Scripts/CutsceneController.cs
+++ Assets/Scripts/CutsceneController.cs
@@ -1,23 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int currentStep = 1;
+    private Animator animator;
+    private string animatorTrigger;
+
+    public Animator arrowAnimator;
+    private bool isShowingArrow = false;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            NextStep();
+        }
     }
 
     public void NextStep()
     {
+        currentStep++;
+
+        if (currentStep == 13)
+        {
+            SceneLoader.Instance.LoadScene("Menu");
+            //SceneManager.LoadScene("Menu");
+        }
+        
+        if (isShowingArrow)
+        {
+            arrowAnimator.SetTrigger("HideArrow");
+            isShowingArrow = false;
+        }
+        animatorTrigger = "Step" + currentStep;
+        animator.SetTrigger(animatorTrigger);
+    }
+
+    public void ShowArrow()
+    {
+        arrowAnimator.SetTrigger("ShowArrow");
+        isShowingArrow = true;
     }
     
 }
